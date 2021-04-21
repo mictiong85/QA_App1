25ed8417b9db082cf342a0c36b6ddc723e31d0f5
mictiong85 <68238173+mictiong85@users.noreply.github.com>
2021-04-21 20:45:15 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/thion/maikeru/qa_app1/Answer.kt
@@ -0,0 +1,4 @@
+package jp.techacademy.thion.maikeru.qa_app1
+import java.io.Serializable
+
+class Answer(val body: String, val name: String, val uid: String, val answerUid: String) : Serializable
---
+++ app/src/main/java/jp/techacademy/thion/maikeru/qa_app1/Const.kt
@@ -0,0 +1,13 @@
+package jp.techacademy.thion.maikeru.qa_app1
+
+// Firebaseにユーザの表示名を保存するパス
+const val UsersPATH = "users"
+
+// Firebaseに質問を保存するバス
+const val ContentsPATH = "contents"
+
+// Firebaseに回答を保存するパス
+const val AnswersPATH = "answers"
+
+// Preferenceに表示名を保存する時のキー
+const val NameKEY = "name"
